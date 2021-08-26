abb216ce38d68a8792b9fec134f094c4f5bb6b99
Jonas <jonas.bruechner@googlemail.com>
2021-08-26 11:50:35 +0200

    inital commit

---
+++ Yii2JavaScriptI18N/src/JSTranslationsCollector.php
@@ -0,0 +1,17 @@
+<?php
+namespace Yii2JavaScriptI18N;
+
+use yii\console\Controller;
+
+/**
+ *
+ * @author devOp
+ */
+class JSTranslationsCollector extends Controller
+{
+
+    public function actionCollect()
+    {
+
+    }
+}
